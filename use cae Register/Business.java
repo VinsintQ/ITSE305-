efa12e664d426b10aa1b05d51fbdccd45c1e694f
Mueen Ali <hejiy2002@gmail.com>
2024-04-24 20:07:03 +0300

    phase 2.3

--- use cae Register/Business.java
+++ use cae Register/Business.java
@@ -23,30 +23,4 @@ public class Business {
 
     }
 
-    /*
-     * public boolean registerUser(String name, String email, String password,
-     * String phoneNumber) {
-     * 
-     * // Create a new user object
-     * User newUser = new User(name, email, password, phoneNumber);
-     * 
-     * success = true;
-     * return success;
-     * }
-     * 
-     * public class User {
-     * private String name;
-     * private String email;
-     * private String password;
-     * private String phoneNumber;
-     * 
-     * public User(String name, String email, String password, String phoneNumber) {
-     * this.name = name;
-     * this.email = email;
-     * this.password = password;
-     * this.phoneNumber = phoneNumber;
-     * }
-     * 
-     * }
-     */
 }
