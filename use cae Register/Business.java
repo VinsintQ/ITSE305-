b555e9ff8d9e84587872473e98758f7b14b7350d
MujtabaSultan <147047265+MujtabaSultan@users.noreply.github.com>
2024-05-14 19:39:56 +0300

    suggestion for register part1

--- use cae Register/Business.java
+++ use cae Register/Business.java
@@ -3,6 +3,8 @@ public class Business {
     // validate inputs
 
     // mathod validate variables is not empty
+    // Improvement suggestion: Renaming the method to 'isEmpty' for clarity
+    // and removing unnecessary if-else statement to simplify the code.
     public boolean Empty(String V) {
         if (V.isEmpty()) {
 
@@ -12,7 +14,8 @@ public class Business {
         }
 
     }
-
+    // Improvement suggestion: Renaming the method to 'NotNum' to be more descriptive,
+    // and simplifying the return statement by directly returning the result of the matches method.
     public boolean NotNum(String Num) {
 
         if (Num.matches("[0-9]+")) {
