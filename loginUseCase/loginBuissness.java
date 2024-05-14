7ca4e01f1bcd3de1c176248bec16b7555af085a5
Ali-Alaalem <147052304+Ali-Alaalem@users.noreply.github.com>
2024-05-14 21:11:21 +0300

    suggestion for mujtaba part

--- loginUseCase/loginBuissness.java
+++ loginUseCase/loginBuissness.java
@@ -1,6 +1,6 @@
 public class loginBuissness {
   public  boolean isAlphanumeric(String username) {
-    // Suggestion: Compile the regular expression as a Pattern object for better performance.
+
       // Regular expression to match alphanumeric characters
       String regex = "^[a-zA-Z0-9]*$";
 
@@ -8,10 +8,11 @@ public class loginBuissness {
       return username.matches(regex);
   }
   public boolean isValidPassword(String password) {
-     // Suggestion: Compile the regular expression as a Pattern object for better performance.
+   
       String regex = "^.{8,16}$";
 
       // Check if the password matches the regular expression
       return password.matches(regex);
   }
 }
+// NO improvement needed for this code
